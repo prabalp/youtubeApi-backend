ae460891acff9149932398bd12d2b4e39e85e00d
prabalp <prabalpanda2008@gmail.com>
2022-06-15 23:33:21 +0530

    added published after as well as save only new data

--- api_scheduler.js
+++ api_scheduler.js
@@ -7,12 +7,22 @@ const api_response = async (API_KEY, searchQuerry) => {
     auth: API_KEY,
   });
   try {
+    //get the latest video from mongoose
+    const video = await Video.find({})
+      .sort({ publishingDateTime: -1 })
+      .limit(1);
+    if (video.length === 0) {
+      var max_date = new Date(2022);
+    } else {
+      var max_date = video[0].publishingDateTime;
+    }
     return youtube.search
       .list({
         part: "snippet",
         q: searchQuerry,
         maxResults: 5,
         order: "date",
+        publishedAfter: max_date,
       })
       .then((res) => {
         // console.log(res.data.items);
@@ -34,13 +44,20 @@ module.exports.api_schedule = async () => {
       const searchQuerry = process.env.SEARCH_QUERRY;
       const response = await api_response(API_KEY, searchQuerry);
       for (let i = 0; i < response.length; i++) {
-        var video = await Video.create({
+        const video = await Video.find({
           title: response[i].snippet.title,
           description: response[i].snippet.description,
-          thumbnail: response[i].snippet.thumbnails.default.url,
-          publishingDateTime: new Date(response[i].snippet.publishedAt),
         });
-        console.log(video);
+
+        if (video.length === 0) {
+          var new_video = await Video.create({
+            title: response[i].snippet.title,
+            description: response[i].snippet.description,
+            thumbnail: response[i].snippet.thumbnails.default.url,
+            publishingDateTime: new Date(response[i].snippet.publishedAt),
+          });
+          console.log(new_video);
+        }
       }
     } catch (error) {
       console.log(error);
